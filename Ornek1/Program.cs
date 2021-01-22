f1004f60a5ba123f3b7e18130a55cc8cafaaa9fc
ugurkryl41 <ugurkarayel41@gmail.com>
2021-01-23 00:57:11 +0300

    Girilen 3 basamaklı sayının basamaklarına ayıran örnek

--- Ornek1/Program.cs
+++ Ornek1/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Ornek1
 {
+    /* Girilen 3 basamaklı sayının basamaklarına ayıran örnek */
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int yuzler = sayi / 100;
+            sayi -= yuzler * 100;
+            int onlar = sayi / 10;
+            sayi -= onlar * 10;
+            int birler = sayi / 1;
+
+            Console.WriteLine("Yuzler: {0} , Onlar: {1} , Birler: {2}",yuzler,onlar,birler);
         }
     }
 }
