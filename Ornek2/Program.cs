fa8586c047882d40f24160483be59172771dbd55
ugurkryl41 <ugurkarayel41@gmail.com>
2021-01-23 02:49:39 +0300

    Girilen Sayının,1 ve kendisi hariç asal çarpanlarının kareleri toplamı

---
+++ Ornek2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace Ornek2
+{
+    class Program
+    {
+        /* Girilen Sayının,1 ve kendisi hariç asal çarpanlarının kareleri toplamı*/
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            ArrayList carpan = new ArrayList();
+            int temp = sayi;           
+            for (int i = 2; i < sayi; i++)
+            {
+                if (temp % i == 0)
+                {
+                    temp /= i;
+                    if (carpan.Contains(i) == false)
+                    {
+                        carpan.Add(i);
+                    }                   
+                }
+            }
+            int toplam = 0;
+            for (int i = 0; i < carpan.Count; i++)
+            {
+                int faktoriyel=1;
+                for (int j = 1; j <= (int)carpan[i]; j++)
+                {
+                    faktoriyel *= j;
+                }
+                toplam += faktoriyel;
+            }
+            Console.WriteLine("Asal çarpanlarının faktoriyel toplamı: {0}",toplam);
+        }
+    }
+}
