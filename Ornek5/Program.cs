527770219a74e3dc0b26ec290efe58a25d1adde1
ugurkryl41 <ugurkarayel41@gmail.com>
2021-01-24 16:51:39 +0300

    Girilen sayının en büyük rakamını bulan kod

---
+++ Ornek5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ornek5
+{
+    class Program
+    {
+        /* Girilen sayının en büyük rakamını bulan kod*/
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            string sayi1 = Convert.ToString(sayi);
+
+            int enb = Convert.ToInt32(sayi1[0].ToString());
+
+            for (int i = 1; i < sayi1.Length; i++)
+            {
+                if (enb<Convert.ToInt32(sayi1[i].ToString()))
+                {
+                    enb = Convert.ToInt32(sayi1[i].ToString());
+                }
+            }
+
+            Console.WriteLine("En Büyük Rakam: "+enb);
+        }
+    }
+}
