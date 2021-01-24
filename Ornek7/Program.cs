8d7ae2a51961472fe991554c62ed7c576f59803c
ugurkryl41 <ugurkarayel41@gmail.com>
2021-01-24 19:30:03 +0300

    1-10 arasında 1000 tane sayının frekansı hesaplayan ornek

---
+++ Ornek7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Ornek7
+{
+    class Program
+    {
+        /* 1-10 arasında 1000 tane sayının frekansı hesaplayan ornek*/
+        static void Main(string[] args)
+        {
+            Random rast = new Random();            
+            int[] sayilar = new int[1000];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = rast.Next(1, 10);
+            }
+            ArrayList liste = new ArrayList();
+            ArrayList frekans = new ArrayList();
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (liste.Contains(sayilar[i]) == false)
+                {
+                    liste.Add(sayilar[i]);
+                    frekans.Add(1);
+                }
+                else
+                {
+                    frekans[liste.IndexOf(sayilar[i])] = Convert.ToInt32(frekans[liste.IndexOf(sayilar[i])]) + 1;
+                }
+            }
+
+            for (int i = 0; i < liste.Count; i++)
+            {
+                Console.WriteLine("{0} Frekans: {1}",liste[i],frekans[i]);
+            }
+
+          
+            
+        }
+    }
+}
