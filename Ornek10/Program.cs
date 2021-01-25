42c04e78be752d82df13f0467d1e4e1bf49d5d8f
ugurkryl41 <ugurkarayel41@gmail.com>
2021-01-26 00:35:11 +0300

    Girilen 3 sayıdan enbüyük ve en küçük bulan kod

---
+++ Ornek10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ornek10
+{
+    class Program
+    {
+        /* Girilen 3 sayıdan enbüyük ve en küçük bulan kod */
+        static void Main(string[] args)
+        {            
+            int[] sayilar = new int[3];
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine("Sayi girin");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                sayilar[i] = sayi;
+            }
+            int enb=Int32.MinValue, enk=Int32.MaxValue;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (sayilar[i]>enb)
+                {
+                    enb = sayilar[i];
+                }
+                if (sayilar[i]<enk)
+                {
+                    enk = sayilar[i];
+                }
+            }
+
+            Console.WriteLine("En Büyük:"+enb+"En küçük:"+enk);
+        }
+    }
+}
