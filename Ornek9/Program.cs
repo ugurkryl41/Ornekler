76ba1dbab1c4ce8bd7f81c3ec73489836715bfdc
ugurkryl41 <ugurkarayel41@gmail.com>
2021-01-26 00:26:58 +0300

    Bir komisyoncu sattığı mallardan kazandığı toplam komisyonu veren ornek

---
+++ Ornek9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ornek9
+{
+    class Program
+    {
+        /* Bir komisyoncu sattığı mallardan kazandığı toplam komisyonu veren ornek 
+         * 50 YTL altındakilerden %3
+         * 50 YTL üzeri ürünlerden %2 komisyon alınımaktadır.
+         */
+        static void Main(string[] args)
+        {
+            double toplamkomisyon=0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Ücreti girin:");
+                double sayi = Convert.ToDouble(Console.ReadLine());
+                double komisyon = sayi >= 50 ? sayi * 2 / 100 : sayi * 3 / 100;
+                Console.WriteLine("Komisyon: "+komisyon);
+                toplamkomisyon += komisyon;
+            }
+            Console.WriteLine("Toplam komisyon"+Math.Round(toplamkomisyon,2));
+
+        }
+    }
+}
