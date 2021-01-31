f5769dc45f85e2ad567380e471329fdb0d888e0d
ugurkryl41 <ugurkarayel41@gmail.com>
2021-02-01 02:50:40 +0300

    Girilen int değeri int tipinde diziye donusturduk.

---
+++ Ornek15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ornek15
+{
+
+    class Program
+    {
+        /* Girilen int değeri int tipinde diziye donusturduk.*/
+        static void Main(string[] args)
+        {
+            Console.Write("Sayi girin");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int[] sayidizi = new int[sayi.ToString().Length];
+            
+            for (int i = 0; i < sayi.ToString().Length; i++)
+            {    
+                int s1 = Convert.ToInt32(sayi.ToString()[i].ToString());
+                sayidizi[i] = s1;
+            }
+
+            foreach (var item in sayidizi)
+            {
+                Console.Write((int)item+",");
+            }
+        }
+
+    }
+}
